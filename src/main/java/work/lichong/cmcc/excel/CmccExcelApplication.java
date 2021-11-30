13deda389ef9d96a8de609e53da2368aa9ab168b
李冲 <git@lichong.host>
2021-11-30 23:21:31 +0800

    Initial commit

---
+++ src/main/java/work/lichong/cmcc/excel/CmccExcelApplication.java
@@ -0,0 +1,13 @@
+package work.lichong.cmcc.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmccExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmccExcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/work/lichong/cmcc/excel/CmccExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package work.lichong.cmcc.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmccExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
